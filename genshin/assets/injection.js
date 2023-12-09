b83849326481db50a7ad462f87154618a02a4094
swwind <i@sww.moe>
2023-12-09 18:34:24 +0800

    add genshin

---
+++ genshin/assets/injection.js
@@ -0,0 +1,14 @@
+// when user clicks right bottom button
+function inject_point_1() {
+  console.log("We should pop a login page!");
+}
+
+// when bridge starts to build
+function inject_point_2() {
+  console.log("We start to load something!!!");
+}
+
+// when user enter the door
+function inject_point_3() {
+  console.log("Somebody enter the door!");
+}
